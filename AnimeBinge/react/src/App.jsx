75588b83577986a3ac717c6efb66fc93d2461d44
Jose Leo Abuan <joseleoabuanjr.01@gmail.com>
2024-04-20 06:46:19 +0800

    Navbar/Leo
    Navigation bar (Logo and Menu)

--- AnimeBinge/react/src/App.jsx
+++ AnimeBinge/react/src/App.jsx
@@ -4,11 +4,36 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      Welcome
+      <nav>
+        <div>AnimeBinge</div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/discover">Discover</Link>
+          </li>
+          <li>
+            <Link to="/about">AboutUs</Link>
+          </li>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/logout">Logout</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div>
+        
+      </div>
     </>
   )
 }
