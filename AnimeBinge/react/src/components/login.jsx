44f3779fe7f3c13c0bb43ba2eeec5bfb4641c973
Jose Leo Abuan <joseleoabuanjr.01@gmail.com>
2024-04-21 00:22:07 +0800

    LoginSignupForm/Leo

--- AnimeBinge/react/src/components/login.jsx
+++ AnimeBinge/react/src/components/login.jsx
@@ -1,10 +1,29 @@
+import { useRef } from "react";
+import { Link } from "react-router-dom";
+
 export default function Login() {
 
-    return (
-      <>
-        <div>
-          Login
-        </div>
-      </>
-    )
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const Submit = useRef();
+ 
+  return (
+    <>
+      <div className="login-signup-form">
+          <div className="form">
+              <h1 className="title">
+                  Login
+              </h1>
+              <form onSubmit={Submit}>
+              <input ref={emailRef} type="email" placeholder="Email" />
+                  <input ref={passwordRef} type="password" placeholder="Password" />
+                  <button className="btn">Login</button>
+                  <p className="message">
+                      Not Registered? <Link to= '/guest/signup'>Create a new account</Link>
+                  </p>
+              </form>
+          </div>
+      </div>
+    </>
+  )
 }
--- AnimeBinge/react/src/components/signup.jsx
+++ AnimeBinge/react/src/components/signup.jsx
@@ -1,10 +1,31 @@
+
+import { useRef } from "react";
+import { Link } from "react-router-dom";
 export default function Signup() {
-    
-    return (
-      <>
-        <div>
-          Signup
-        </div>
-      </>
-    )
+
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const Submit = useRef();
+
+  return (
+    <>
+      <div className="login-signup-form">
+          <div className="form">
+              <h1 className="title">
+                  Sign Up
+              </h1>
+              <form onSubmit={Submit}>
+                  <input ref={nameRef} type="username" placeholder="Username" />
+                  <input ref={emailRef} type="email" placeholder="Email" />
+                  <input ref={passwordRef} type="password" placeholder="Password" />
+                  <button className="btn">Sign Up</button>
+                  <p className="message">
+                      Already Have An Account? <Link to= '/guest/login'>Login</Link>
+                  </p>
+              </form>
+          </div>
+      </div>
+    </>
+  )
 }
