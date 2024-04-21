a0aa3d210bfc5268e61a77e54e754b5794177dd4
Jose Leo Abuan <joseleoabuanjr.01@gmail.com>
2024-04-22 01:00:47 +0800

    Display a list of anime/ Leo

---
+++ AnimeBinge/react/src/views/animelist.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import axiosClient from "../axios-client.js";
+import { Link } from "react-router-dom";
+
+export default function AnimeList() {
+  const [anime, setAnimes] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getAnime();
+  }, [])
+
+  const getAnime = () => {
+    setLoading(true)
+    axiosClient.get('/animes')
+      .then(({ data }) => {
+        setLoading(false)
+        setAnimes(data.data)
+      })
+      .catch(() => {
+        setLoading(false)
+      })
+  }
+
+  anime.map(item => (
+    item.id,
+    item.name,
+    item.rating,
+    item.category,
+    item.description
+  ))
+
+  return (
+    <div>
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <h1>Discover Anime</h1>
+        <Link className="btn-add" to="/discover/addanime">Add Anime</Link>
+      </div>
+      <div className="card animated fadeInDown">
+        {loading && <div className="loading">Loading...</div>}
+        {!loading && (
+          <div className="card-container">
+            {anime.map((item) => (
+              <div key={item.id} className="card-item">
+                <h2>{item.name}</h2>
+                <p>Rating: {item.rating}</p>
+                <p>Category: {item.category}</p>
+                <p>Description: {item.description}</p>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
